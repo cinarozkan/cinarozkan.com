363f6cfa757212e668c67d2742d57a899367baaa
cinarozkan <110178368+cinarozkan@users.noreply.github.com>
2025-05-04 13:07:29 +0300

    Changes to under construction page nad 404 page.
    1- Added small text at the bottom of the page for the important links and information.
    2- Added live work status.
    3- Increased mobile compatibility.

--- script.js
+++ script.js
@@ -17,7 +17,16 @@ yaziYenile();
 
 let tahminiZaman = 0;
 if (tahminiZaman === 0) {
-    document.getElementById("tahminiZaman").innerText = "Estimated Time To Finish: N/A";
+    document.getElementById("tahminiZaman").innerText = "🕒Estimated Time To Finish: N/A";
 } else {
-    document.getElementById("tahminiZaman").innerText = "";
+    document.getElementById("tahminiZaman").innerText = "🕒Estimated Time To Finish: ";
+}
+
+let LiveWork = 0; // 0 = No, 1 = Yes
+const lastWorkDateTime = "04.05.2025 12.59" // Input last work time and date. Only visible if LiveWork = 0. Format: DD.MM.YYYY HH:MM (UTC+3.00)
+if (LiveWork === 0) {
+    document.getElementById("LiveWork").innerHTML = '🔴Am I Working on the Website <span class="Underline">Right Now?</span> --> No.🚫';
+    document.getElementById("lastWorkTime").innerHTML = "☕The Last Time I Was Working: " + lastWorkDateTime + " (UTC+3.00)";
+} else {
+    document.getElementById("LiveWork").innerHTML = '🔴Am I Working on the Website <span class="Underline">Right Now?</span> --> Yes.🧑‍💻✅';
 }
